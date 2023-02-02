957f49da955a73271242ba671a9dfe9b52307a5f
jukuly <vpare070@uottawa.ca>
2023-02-01 23:55:35 -0500

    add home page

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export default function () {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,10 @@
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <App />
 );
