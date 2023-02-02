d602023ad8ebb530427561f80aa1e222880bc4b7
jukuly <vpare070@uottawa.ca>
2023-02-02 00:00:56 -0500

    added home page 2/2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
 
 export default function () {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
